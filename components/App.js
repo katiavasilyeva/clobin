c94f3b3432463131a4c7776cb294d3fde8489cae
katiavasilyeva <vasilyeva64@gmail.com>
2017-02-16 15:38:28 -0500

    marker component  added

--- components/App.js
+++ components/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import Map from './map'
+import MapContainer from './MapContainer'
+
 
 class App extends Component{
     render(){
         return(
             <div>
-                <Map/>
+                <MapContainer/>
             </div>
 
         )
