731c2b4769ef4d8fbc989f9d633b0bfa09593b4e
katiavasilyeva <vasilyeva64@gmail.com>
2017-02-17 10:11:18 -0500

    firebase added

--- entry.js
+++ entry.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
-
-
+import firebase from 'firebase';
+const config = {
+    apiKey: "AIzaSyBZam93AT_8zj8yCrTlGFAZm4ORD0PIVYU",
+    authDomain: "clobin-91825.firebaseapp.com",
+    databaseURL: "https://clobin-91825.firebaseio.com",
+    storageBucket: "clobin-91825.appspot.com",
+    messagingSenderId: "288918706586"
+};
+firebase.initializeApp(config);
 ReactDOM.render(<App/>, document.getElementById("placeholder"));
